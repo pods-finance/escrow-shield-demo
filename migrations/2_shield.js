54d11056c8e0d829da18947cad98e2ca3df53225
Papais <8097453+LeoPapais@users.noreply.github.com>
2024-04-24 19:18:58 -0300

    fix backticks

--- migrations/2_shield.js
+++ migrations/2_shield.js
@@ -16,7 +16,7 @@ const vkInput = [];
 let vk = [];
 functionNames.forEach((name) => {
 	const vkJson = JSON.parse(
-		fs.readFileSync(/app/orchestration/common/db/${name}_vk.key, "utf-8")
+		fs.readFileSync(`/app/orchestration/common/db/${name}_vk.key`, "utf-8")
 	);
 	if (vkJson.scheme) {
 		vk = Object.values(vkJson).slice(2).flat(Infinity);
