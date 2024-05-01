cd28322a5d3b609086dde89bb9785e57687cc4e6
Robsonsjre <robsonsjre@gmail.com>
2024-05-01 07:52:56 -0300

    feat: added timber request to deposit test

--- starlight-deployer/test/scenarios/scenario1.test.js
+++ starlight-deployer/test/scenarios/scenario1.test.js
@@ -14,6 +14,7 @@ describe('Full scenario - (depositA / depositA / depositB / depositB / Transfer 
     accountBankA = '0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266'
     orchestrationURLBankA = 'http://localhost:3000'
     orchestrationURLBankB = 'http://localhost:3000'
+    timberURLBankA = 'http://localhost:3100'
 
     erc20 = await hre.ethers.getContractAt('contracts/ERC20.sol:ERC20', erc20Address)
     await erc20.connect(bankASigner).approve(escrowShieldAddress, 10000000000)
@@ -30,7 +31,7 @@ describe('Full scenario - (depositA / depositA / depositB / depositB / Transfer 
     
   })
 
-  it('Should BankA deposit 100 units to EscrowShield', async () => {
+  it('Should BankA deposit 100 units to EscrowShield correctly', async () => {
     const allowance = await erc20.allowance(accountBankA, escrowShieldAddress);
     const balanceOf = await erc20.balanceOf(accountBankA);
     expect(allowance).to.greaterThan(0)
@@ -41,6 +42,35 @@ describe('Full scenario - (depositA / depositA / depositB / depositB / Transfer 
     const response = await axios.post(`${orchestrationURLBankA}/deposit`, { amount: INITIAL_AMOUNT})
     expect(response.status).to.equal(200)
     expect(response.data.tx.event).to.equal('NewLeaves')
+
+    const responseTimber = await axios.get(`${timberURLBankA}/getAllCommitments`)
+    console.log('responseTimber', responseTimber.data)
+    
   });
+
+  // it('Should BankB deposit 100 units to EscrowShield correctly', async () => {
+  //   const allowance = await erc20.allowance(accountBankB, escrowShieldAddress);
+  //   const balanceOf = await erc20.balanceOf(accountBankB);
+  //   expect(allowance).to.greaterThan(0)
+  //   expect(balanceOf).to.greaterThan(0)
+
+  //   const INITIAL_AMOUNT = 100
+
+  //   const response = await axios.post(`${orchestrationURLBankB}/deposit`, { amount: INITIAL_AMOUNT})
+  //   expect(response.status).to.equal(200)
+  //   expect(response.data.tx.event).to.equal('NewLeaves')
+  // });
+
+  // it('Should BankA transfer 50 units to BankB correctly', async () => {
+  //   const INITIAL_AMOUNT = 50
+  //   const data = {
+  //     "recipient": "0xCeDEFc245eA0f0f0e036F888ada190D6eDb33203"
+  //     "amount": INITIAL_AMOUNT
+  //   }
+
+  //   const response = await axios.post(`${orchestrationURLBankA}/transfer`, data)
+  //   expect(response.status).to.equal(200)
+  //   expect(response.data.tx.event).to.equal('NewLeaves')
+  // });
 
 });
